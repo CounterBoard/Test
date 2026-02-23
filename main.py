32cb9ea13dd6bdf720d8f34b9b6b4fe598981d20
CounterBoard <aleksejletunov23@gmail.com>
2026-02-23 20:17:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import requests
 
 TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
-RENDER_URL = "https://akuxfafueruirffuax.onrender.com"  # замени на свой URL
+RENDER_URL = "https://test-tys3.onrender.com"  # замени на свой URL
 
 url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/setWebhook"
 data = {"url": f"{RENDER_URL}/"}
 
 response = requests.post(url, json=data)
 print("✅ Вебхук настроен:", response.json())
+
